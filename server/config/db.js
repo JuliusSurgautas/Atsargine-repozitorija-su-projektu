344d72cab38c82685f5a6e0d212c3e0ea6a58055
edv1nn <capledvin@gmail.com>
2024-10-11 17:07:24 +0300

    fixed database connection

--- server/config/db.js
+++ server/config/db.js
@@ -1,9 +1,17 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const connectDB = async () => {
-  await mongoose
-    .connect(
-      "mongodb+srv://test:test123@cluster0.fvbni.mongodb.net/coffee-shop"
-    )
-    .then(() => console.log("DB Connected"));
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("DB Connected");
+  } catch (error) {
+    console.error("DB Connection Error:", error.message);
+    process.exit(1);
+  }
 };
