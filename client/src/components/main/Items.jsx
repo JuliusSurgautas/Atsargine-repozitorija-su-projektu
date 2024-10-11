cb8208eea8740e443ad0fd2c844cebf11608d464
Eidanas Rynkevicius <eidanas.ryn@gmail.com>
2024-10-11 17:33:56 +0300

    added top rated category

--- client/src/components/main/Items.jsx
+++ client/src/components/main/Items.jsx
@@ -1,5 +1,6 @@
 import styles from "./main.module.css";
 
+import { TopRatedItems } from "../../data/data";
 import { useState } from "react";
 import { OurItems } from "../../data/data";
 import { Link } from "react-router-dom";
@@ -13,8 +14,23 @@ const Items = () => {
 
   return (
     <section className={styles.items}>
+      <div className="small-containerEmpty">
+      <h2 className={styles.items__title}>Geriausi pasiūlymai</h2>
+      <div className={styles.items__row}>
+      {TopRatedItems.slice(0, visibleItems).map((item) => (
+            <div key={item.id} className={styles.items__product}>
+              <Link to={item.to} className={styles.items__image}>
+                <img src={item.image} alt={item.title} />
+              </Link>
+              <h4 className={styles.items__product_title}>{item.title}</h4>
+              <p className={styles.items__product_price}>{item.price}</p>
+            </div>
+          ))}
+      </div>
+      </div>
+
       <div className="small-container">
-        <h2 className={styles.items__title}>Populiariausi skelbimai</h2>
+        <h2 className={styles.items__title}>Visi Skelbimai</h2>
         <div className={styles.items__row}>
           {OurItems.slice(0, visibleItems).map((item) => (
             <div key={item.id} className={styles.items__product}>
--- client/src/data/data.js
+++ client/src/data/data.js
@@ -46,6 +46,79 @@ import itemImage31 from "../images/items/beata1.webp";
 import itemImage32 from "../images/items/beata4.webp";
 import itemImage33 from "../images/items/beata5.webp";
 
+import item9 from "../images/items/BlackCrow.webp"
+import itemImage34 from "../images/items/BlackCrow2.webp"
+import itemImage35 from "../images/items/Blackcrow3.webp"
+import itemImage36 from "../images/items/BlackCrow4.webp"
+import itemImage37 from "../images/items/BlackCrow5.webp"
+
+import item10 from "../images/items/Royale.webp"
+import itemImage38 from "../images/items/Royale2.webp"
+import itemImage39 from "../images/items/Royale3.webp"
+import itemImage40 from "../images/items/Royale4.webp"
+import itemImage41 from "../images/items/Royale5.webp"
+
+
+import item11 from "../images/items/Intenso.webp"
+import itemImage42 from "../images/items/Intenso2.webp"
+import itemImage43 from "../images/items/Intenso3.webp"
+import itemImage44 from "../images/items/Intenso4.webp"
+import itemImage45 from "../images/items/Intenso5.webp"
+
+import item12 from "../images/items/Dolce.webp"
+import itemImage46 from "../images/items/dolce2.webp"
+import itemImage47 from "../images/items/dolce3.webp"
+import itemImage48 from "../images/items/dolce4.webp"
+import itemImage49 from "../images/items/dolce5.webp"
+
+import item13 from "../images/items/Parralel17Gift.webp"
+import itemImage50 from "../images/items/Parallel17Gift2.webp"
+import itemImage51 from "../images/items/Parallel17Gift3.webp"
+import itemImage52 from "../images/items/Parallel17Gift4.webp"
+import itemImage53 from "../images/items/Parallel17Gift5.webp"
+
+import item14 from "../images/items/Amicosso.webp"
+import itemImage54 from "../images/items/Amicosso2.webp"
+import itemImage55 from "../images/items/Amicosso3.webp"
+import itemImage56 from "../images/items/amicosso4.webp"
+import itemImage57 from "../images/items/amicosso5.webp"
+
+import item15 from "../images/items/christmas.webp"
+import itemImage58 from "../images/items/christmas2.webp"
+import itemImage59 from "../images/items/christmas3.webp"
+import itemImage60 from "../images/items/christmas4.webp"
+import itemImage61 from "../images/items/christmas5.webp"
+
+import item16 from "../images/items/Lullaby.webp"
+import itemImage62 from "../images/items/lullaby2.webp"
+import itemImage63 from "../images/items/lullaby4.webp"
+import itemImage64 from "../images/items/Lullaby5.webp"
+import itemImage65 from "../images/items/Lullaby.webp"
+
+import item17 from "../images/items/Parallel12Gift.webp"
+import itemImage66 from "../images/items/parallel12gift2.webp"
+import itemImage67 from "../images/items/parallel12Gift3.webp"
+import itemImage68 from "../images/items/parallel12Gift4.webp"
+import itemImage69 from "../images/items/parallel12Gift5.webp"
+
+import item18 from "../images/items/DolceGusto.webp"
+import itemImage70 from "../images/items/DolceGusto2.webp"
+import itemImage71 from "../images/items/DolceGusto3.webp"
+import itemImage72 from "../images/items/DolceGusto4.webp"
+import itemImage73 from "../images/items/DolceGusto5.webp"
+
+import item19 from "../images/items/Chiato.webp"
+import itemImage74 from "../images/items/Chiato2.webp"
+import itemImage75 from "../images/items/Chiato3.webp"
+import itemImage76 from "../images/items/Chiato4.webp"
+import itemImage77 from "../images/items/chiato5.webp"
+
+import item20 from "../images/items/Amaretto.webp"
+import itemImage78 from "../images/items/amaretto2.webp"
+import itemImage79 from "../images/items/amaretto3.webp"
+import itemImage80 from "../images/items/Amaretto4.webp"
+import itemImage81 from "../images/items/amaretto5.webp"
+
 export const OurItems = [
   {
     id: 1,
@@ -207,25 +280,25 @@ export const OurItems = [
     to: "/productdetail/8",
   },
   {
-    id: 8,
-    image: item8,
-    toptitle: "Malta Kava / BEATOS VIRTUVĖ",
-    secondtitle: "Beatos Virtuvė, 500 g",
-    title: "Malta kava BEATOS VIRTUVĖ, 500 g",
+    id: 9,
+    image: item9,
+    toptitle: "Malta Kava / Black Crow White Pigeon",
+    secondtitle: "Black Crow White Pigeon Ethiopia Burtukaana, 1 kg",
+    title: "Rūšinės kavos pupelės Black Crow White Pigeon Ethiopia Burtukaana, 1 kg",
     list: [
-      "Kavos degustacijos metu Beata išragavo kelias dešimtis ",
-      "įvairiausių rūšių ir skonių kavų iš skirtingų pasaulio ",
-      "kampelių. Paklausta, kokio skonio ieško, pasidalino: nei ",
-      "pernelyg rūgštaus, nei pernelyg kartaus…tokio, kuriame ",
-      "harmoningai suskamba visi skoniai ir aromatai – kaip ir gerai paruoštame patiekale.",
+      "Visur pakvimpa miško mėlynėmis, skleidžiasi rabarbarų ir saldžios vanilės natos ",
+      "Sklandant kelių tūkstančių metrų aukštyje, virš Etiopijos Guji regiono, žvilgsnis stabteli už sraunios Turo upės ir šalia įsikūrusios kavos apdorojimo stoties „Anasora“ ",
+      "Į šią stotį derlių suneša 1000 apylinkėse gyvenančių smulkių ūkininkų, priklausančių Oroma etninei grupei. Apdorojimo stoties šeimininkas ",
+      "Skonio paletė | Rabarbarai, mėlynės, vanilė ",
+      "Rekomenduojami kavos ruošimo būdai: visi ruošimo būdai, taip pat ir espresso.",
     ],
     price: "14,90 €",
-    images: [itemImage30, itemImage31, itemImage32, itemImage33],
-    to: "/productdetail/8",
+    images: [itemImage34, itemImage35, itemImage36, itemImage37],
+    to: "/productdetail/9",
   },
   {
-    id: 8,
-    image: item8,
+    id: 10,
+    image: item10,
     toptitle: "Malta Kava / BEATOS VIRTUVĖ",
     secondtitle: "Beatos Virtuvė, 500 g",
     title: "Malta kava BEATOS VIRTUVĖ, 500 g",
@@ -237,12 +310,12 @@ export const OurItems = [
       "harmoningai suskamba visi skoniai ir aromatai – kaip ir gerai paruoštame patiekale.",
     ],
     price: "14,90 €",
-    images: [itemImage30, itemImage31, itemImage32, itemImage33],
-    to: "/productdetail/8",
+    images: [itemImage38, itemImage39, itemImage40, itemImage41],
+    to: "/productdetail/10",
   },
   {
-    id: 8,
-    image: item8,
+    id: 11,
+    image: item11,
     toptitle: "Malta Kava / BEATOS VIRTUVĖ",
     secondtitle: "Beatos Virtuvė, 500 g",
     title: "Malta kava BEATOS VIRTUVĖ, 500 g",
@@ -254,12 +327,12 @@ export const OurItems = [
       "harmoningai suskamba visi skoniai ir aromatai – kaip ir gerai paruoštame patiekale.",
     ],
     price: "14,90 €",
-    images: [itemImage30, itemImage31, itemImage32, itemImage33],
-    to: "/productdetail/8",
+    images: [itemImage42, itemImage43, itemImage44, itemImage45],
+    to: "/productdetail/11",
   },
   {
-    id: 8,
-    image: item8,
+    id: 12,
+    image: item12,
     toptitle: "Malta Kava / BEATOS VIRTUVĖ",
     secondtitle: "Beatos Virtuvė, 500 g",
     title: "Malta kava BEATOS VIRTUVĖ, 500 g",
@@ -271,12 +344,12 @@ export const OurItems = [
       "harmoningai suskamba visi skoniai ir aromatai – kaip ir gerai paruoštame patiekale.",
     ],
     price: "14,90 €",
-    images: [itemImage30, itemImage31, itemImage32, itemImage33],
-    to: "/productdetail/8",
+    images: [itemImage46, itemImage47, itemImage48, itemImage49],
+    to: "/productdetail/12",
   },
   {
-    id: 8,
-    image: item8,
+    id: 13,
+    image: item13,
     toptitle: "Malta Kava / BEATOS VIRTUVĖ",
     secondtitle: "Beatos Virtuvė, 500 g",
     title: "Malta kava BEATOS VIRTUVĖ, 500 g",
@@ -288,12 +361,12 @@ export const OurItems = [
       "harmoningai suskamba visi skoniai ir aromatai – kaip ir gerai paruoštame patiekale.",
     ],
     price: "14,90 €",
-    images: [itemImage30, itemImage31, itemImage32, itemImage33],
-    to: "/productdetail/8",
+    images: [itemImage50, itemImage51, itemImage52, itemImage53],
+    to: "/productdetail/13",
   },
   {
-    id: 8,
-    image: item8,
+    id: 14,
+    image: item14,
     toptitle: "Malta Kava / BEATOS VIRTUVĖ",
     secondtitle: "Beatos Virtuvė, 500 g",
     title: "Malta kava BEATOS VIRTUVĖ, 500 g",
@@ -305,12 +378,12 @@ export const OurItems = [
       "harmoningai suskamba visi skoniai ir aromatai – kaip ir gerai paruoštame patiekale.",
     ],
     price: "14,90 €",
-    images: [itemImage30, itemImage31, itemImage32, itemImage33],
-    to: "/productdetail/8",
+    images: [itemImage54, itemImage55, itemImage56, itemImage57],
+    to: "/productdetail/14",
   },
   {
-    id: 8,
-    image: item8,
+    id: 15,
+    image: item15,
     toptitle: "Malta Kava / BEATOS VIRTUVĖ",
     secondtitle: "Beatos Virtuvė, 500 g",
     title: "Malta kava BEATOS VIRTUVĖ, 500 g",
@@ -322,12 +395,12 @@ export const OurItems = [
       "harmoningai suskamba visi skoniai ir aromatai – kaip ir gerai paruoštame patiekale.",
     ],
     price: "14,90 €",
-    images: [itemImage30, itemImage31, itemImage32, itemImage33],
-    to: "/productdetail/8",
+    images: [itemImage58, itemImage59, itemImage60, itemImage61],
+    to: "/productdetail/15",
   },
   {
-    id: 8,
-    image: item8,
+    id: 16,
+    image: item16,
     toptitle: "Malta Kava / BEATOS VIRTUVĖ",
     secondtitle: "Beatos Virtuvė, 500 g",
     title: "Malta kava BEATOS VIRTUVĖ, 500 g",
@@ -339,12 +412,12 @@ export const OurItems = [
       "harmoningai suskamba visi skoniai ir aromatai – kaip ir gerai paruoštame patiekale.",
     ],
     price: "14,90 €",
-    images: [itemImage30, itemImage31, itemImage32, itemImage33],
-    to: "/productdetail/8",
+    images: [itemImage62, itemImage63, itemImage64, itemImage65],
+    to: "/productdetail/16",
   },
   {
-    id: 8,
-    image: item8,
+    id: 17,
+    image: item17,
     toptitle: "Malta Kava / BEATOS VIRTUVĖ",
     secondtitle: "Beatos Virtuvė, 500 g",
     title: "Malta kava BEATOS VIRTUVĖ, 500 g",
@@ -356,12 +429,12 @@ export const OurItems = [
       "harmoningai suskamba visi skoniai ir aromatai – kaip ir gerai paruoštame patiekale.",
     ],
     price: "14,90 €",
-    images: [itemImage30, itemImage31, itemImage32, itemImage33],
-    to: "/productdetail/8",
+    images: [itemImage66, itemImage67, itemImage68, itemImage69],
+    to: "/productdetail/17",
   },
   {
-    id: 8,
-    image: item8,
+    id: 18,
+    image: item18,
     toptitle: "Malta Kava / BEATOS VIRTUVĖ",
     secondtitle: "Beatos Virtuvė, 500 g",
     title: "Malta kava BEATOS VIRTUVĖ, 500 g",
@@ -373,12 +446,12 @@ export const OurItems = [
       "harmoningai suskamba visi skoniai ir aromatai – kaip ir gerai paruoštame patiekale.",
     ],
     price: "14,90 €",
-    images: [itemImage30, itemImage31, itemImage32, itemImage33],
-    to: "/productdetail/8",
+    images: [itemImage70, itemImage71, itemImage72, itemImage73],
+    to: "/productdetail/18",
   },
   {
-    id: 8,
-    image: item8,
+    id: 19,
+    image: item19,
     toptitle: "Malta Kava / BEATOS VIRTUVĖ",
     secondtitle: "Beatos Virtuvė, 500 g",
     title: "Malta kava BEATOS VIRTUVĖ, 500 g",
@@ -390,7 +463,107 @@ export const OurItems = [
       "harmoningai suskamba visi skoniai ir aromatai – kaip ir gerai paruoštame patiekale.",
     ],
     price: "14,90 €",
-    images: [itemImage30, itemImage31, itemImage32, itemImage33],
-    to: "/productdetail/8",
+    images: [itemImage74, itemImage75, itemImage76, itemImage77],
+    to: "/productdetail/19",
+  },
+  {
+    id: 20,
+    image: item20,
+    toptitle: "Malta Kava / BEATOS VIRTUVĖ",
+    secondtitle: "Beatos Virtuvė, 500 g",
+    title: "Malta kava BEATOS VIRTUVĖ, 500 g",
+    list: [
+      "Kavos degustacijos metu Beata išragavo kelias dešimtis ",
+      "įvairiausių rūšių ir skonių kavų iš skirtingų pasaulio ",
+      "kampelių. Paklausta, kokio skonio ieško, pasidalino: nei ",
+      "pernelyg rūgštaus, nei pernelyg kartaus…tokio, kuriame ",
+      "harmoningai suskamba visi skoniai ir aromatai – kaip ir gerai paruoštame patiekale.",
+    ],
+    price: "14,90 €",
+    images: [itemImage78, itemImage79, itemImage80, itemImage81],
+    to: "/productdetail/20",
+  },
+];
+
+export const TopRatedItems = [
+  {
+    id: 1,
+    image: item1,
+    toptitle: "Malta Kava / Peach Fuzz",
+    secondtitle: "Peach Fuzz",
+    title:
+      "Riboto leidimo malta kava Coffee of the Year 2024 Peach Fuzz, 200 g ",
+    list: [
+      "Šalis | Kolumbija.",
+      "Regionas | Huila",
+      "Ūkis | Rodrigo Sanchezo valdomas „Monteblanco“ ūkis",
+      "Augimo aukštis | 1730 m",
+      "Arabikos porūšis | Bourbon, Caturra",
+      "Apdorojimas | Drėgnasis, fermentuota su persikais",
+      "Skrudinimas | Vidutinis",
+      "Skonio paletė | Persikai, mangai, karamelė",
+    ],
+    price: "13,99 €",
+    images: [itemImage1, itemImage2, itemImage3, itemImage4],
+    to: "/productdetail/1",
+  },
+
+  {
+    id: 2,
+    image: item2,
+    toptitle: "Malta Kava / Caprisette Fragrante",
+    secondtitle: "Caprisette Fragrante, 250 g",
+    title: "Malta kava Caprisette Fragrante, 250 g",
+    list: [
+      "Caprisette kava milijonų jau pamėgta. O tu ar ragavai?",
+      "Unikalūs skonių mišiniai, sukurti skanios kasdienės kavos ",
+      "puodeliui, jau užkariavo daugelio kavos mėgėjų širdis. ",
+      "Sodraus espresso ar kreminės latės? Mes paskrudinome ",
+      "taip, kad tiktų visiems tavo mėgstamiems receptams. ",
+      "Pasinerk į Caprisette skonių pasaulį ir atrask savo mėgstamiausią.",
+      "Milijonai jau pamėgo patiks ir tau!",
+      "Sudedamosios dalys: malta skrudinta kava.",
+      "Laikymo sąlygos: laikyti vėsioje vietoje, atokiau nuo saulės spindulių. ",
+    ],
+    price: "8,90 €",
+    images: [itemImage6, itemImage7, itemImage8, itemImage9],
+    to: "/productdetail/2",
+  },
+
+  {
+    id: 3,
+    image: item3,
+    toptitle: "Malta Kava / Caprisette Belgique",
+    secondtitle: "Caprisette Belgique, 250 g",
+    title: "Caprisette Belgique, 250 g",
+    list: [
+      "Brazilijos ir Hondūro kavų mišinys (80 % arabikos ir 20 % ",
+      "robustos) patiks tradicinio kavos skonio mėgėjams.",
+      "Intensyvaus aromato, tvirto „kūno“ kava. Palieka ilgai",
+      "išliekantį juodojo šokolado ir saldžios karamelės poskonį.",
+    ],
+    price: "8,90 €",
+    images: [itemImage10, itemImage11, itemImage12, itemImage13],
+    to: "/productdetail/3",
+  },
+
+  {
+    id: 4,
+    image: item4,
+    toptitle: "Malta Kava / Caprisette Fragrante",
+    secondtitle: "Parallel 36, 250 g",
+    title: "Malta kava Parallel 36, 250 g",
+    list: [
+      "Pajuskite Amerikos skonius savo puodelyje! „Parallel 36“ –",
+      "tai arabikos pupelių mišinys, kuriame puikiai dera rūšinės",
+      "kavos pupelės iš Salvadoro ir Brazilijos. Sodraus aromato",
+      "kava, kurios skonyje atsiskleidžia šokolado, žemės riešutų,",
+      "medienos ir gėlių natos.",
+      "Sudedamosios dalys: malta skrudinta kava.",
+      "Laikymo sąlygos: laikyti sausoje, vėsioje vietoje.",
+    ],
+    price: "10,50 €",
+    images: [itemImage14, itemImage15, itemImage16, itemImage17],
+    to: "/productdetail/4",
   },
 ];
